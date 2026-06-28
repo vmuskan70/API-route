fceecfaf80c4e6f3fde0117f56c8f514a9cfd3da
vmuskan70 <vmuskan709@gmail.com>
2026-06-28 22:42:13 +0530

    new

---
+++ index.js
@@ -0,0 +1,61 @@
+const express=require("express");
+const app=express();
+app.use(express.json())
+let students=[
+    {id:1,name:"muskan",city:"gkp"},
+    {id:2,name:"janhavi",city:"skp"}
+];
+app.get("/api/students",(req,res)=>{
+    res.json(students);
+});
+app.get("/api/students/:id",(req,res)=>{
+    const {id}=req.params;
+    const student=students.find(s=>s.id ==id);
+    if(!student){
+        return res.status(404).json({message:"invalid id"});
+    }
+    res.json(student);
+});
+app.post("/api/students",(req,res)=>{
+    console.log(req.body);
+    const {id,name,city}=req.body;
+    const newStudent={
+        id:id,
+        name:name,
+        city:city
+    };
+    students.push(newStudent);
+    res.json({
+        message:"student added successfully!!",
+           data:req.body,
+            student:students
+    });
+});
+//updated records
+app.put("/api/students/:id",(req,res)=>{
+    const {id}=req.params;
+    const student=students.find(s=>s.id ==id);
+    if(!student){
+        return res.json({
+            message:"invalid students id"
+        });
+    }
+    student.name=req.body.name;
+    student.city=req.body.city;
+    res.json({
+        message:"student record updated",
+        student
+    });
+});
+//delete
+app.delete("/api/students/:id",(req,res)=>{
+    const {id}=req.params;
+    students=students.filter(s=>s.id !=id);
+    res.json({
+        message:"record deleted...",
+        data:students
+    });
+});
+app.listen(5000,()=>{
+    console.log("server started....")
+});
